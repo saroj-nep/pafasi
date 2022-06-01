ce2b93dc0bd6017b1916c32b4d178f867b8d0c77
saroj-nep <sarojapr29@gmail.com>
2022-06-01 12:26:35 +0200

    some styling changes

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -2,13 +2,13 @@
      <header>
         <h1>PaFaSi<br/>Uni-Homburg</h1>
       </header>
-    
-    <router-link to="/einlei"><Button text="Einleitung" color="red"/></router-link>
-    <router-link to="/anlei"><br><Button text="Anleitung Zu App" color="red"/></router-link>
-    <router-link to="/wart"><br><Button text="Wartezimmer" color="red"/></router-link>
-    <router-link to="/tip"><br><Button text="Tipps & Links" color="red"/></router-link>
-    <router-link to="/kleinge"><br><Button text="Kleingedrucktes" color="red"/></router-link>
-
+    <div class="menu">
+        <router-link to="/einlei"><br><Button text="Einleitung" color="#42b983"/></router-link>
+        <router-link to="/anlei"><br><Button text="Anleitung Zu App" color="#42b983"/></router-link>
+        <router-link to="/wart"><br><Button text="Wartezimmer" color="#42b983"/></router-link>
+        <router-link to="/tip"><br><Button text="Tipps & Links" color="#42b983"/></router-link>
+        <router-link to="/kleinge"><br><Button text="Kleingedrucktes" color="#42b983"/></router-link>
+    </div>
     
 </template>
 
@@ -22,3 +22,23 @@ export default{
     }
 }
 </script>
+
+<style>
+.h1{
+ border-bottom: 1px solid#ddd;
+ display:inline-block;
+ padding:10px;   
+}
+
+.menu button {
+background: #f4f4f4;
+   padding: 20px;
+   border-radius: 10px;
+   margin: 5px auto ;
+   max-width: 50 px;
+   width:20%;
+   cursor: pointer;
+   color: #444  
+} 
+
+</style>
