a6cf31d452ff9bed2f25b6f3e8ed4a427171309f
saroj-nep <sarojapr29@gmail.com>
2022-08-08 17:50:19 +0200

    removed unnecessary files

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,6 @@ import EinleitungView from '../views/EinleitungView.vue'
 import Wartezimmer from '../views/Wartezimmer.vue'
 import Login from '../views/LoginPage.vue'
 import Patient from '../views/patients/PatientProfile.vue'
-import Anamnesemenu from '../views/menu/Anamnesemenu.vue'
-import Facharztmenu from '../views/menu/Facharztmenu.vue'
-import Labaratorymenu from '../views/menu/Labaratorymenu.vue'
-import Patientenaktemenu from '../views/menu/Patientenaktemenu.vue'
-import Untersuchenmenu from '../views/menu/Untersuchenmenu.vue'
-import Submitmenu from '../views/menu/Submitmenu.vue'
 import ErrorPage from "@/views/ErrorPage.vue"
 
 
@@ -39,26 +33,7 @@ const routes = [
     path: '/patients/:id',name: 'Patient',component: Patient, props:true
   },
   
-  {
-    path: '/anamnese',name: 'Anamnese',component: Anamnesemenu, 
-  },
-
-  {
-    path: '/patientenakte',name: 'Patientenakte',component: Patientenaktemenu, 
-  },
-  {
-    path: '/untersuchen',name: 'Untersuchen',component: Untersuchenmenu,
-  },
-  {
-    path: '/labaratory',name: 'Labaratory',component: Labaratorymenu, 
-  },
-  {
-    path: '/facharzt',name: 'Facharzt',component: Facharztmenu, 
-  },
   
-  {
-    path: '/submit',name: 'Submit',component: Submitmenu, 
-  },
   {
     path: "/:catchAll(.*)",
     name: "ErrorPage",
