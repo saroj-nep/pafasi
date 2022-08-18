f17d6ecec26966873b0ac029372f0f7748ba7f5d
saroj-nep <sarojapr29@gmail.com>
2022-08-18 16:24:41 +0200

    working api for login

---
+++ src/Api/db.php
@@ -0,0 +1,4 @@
+<?php
+$conn = new mysqli("localhost","root","","db");
+if (!$conn){echo "Error, db not connected.";}
+?>
--- src/components/Login/loginForm.vue
+++ src/components/Login/loginForm.vue
@@ -1,69 +1,44 @@
 <template>
-  <form action="" class="form" @submit="checkLogin" method="POST">
-    <div class="errorLogin disable">
-      <p class="errorLogin__text" id="errorMsg">
-        Ihre E-Mail und/oder Ihr Passwort sind falsch.
-      </p>
-    </div>
-    <div class="form__group">
-      <label for="Email" class="form__label">Studenten-E-Mail:</label>
-      <input
-        type="email"
-        class="form__input"
-        :class="{
-          valid: emailValid,
-          notValid: !emailValid,
-        }"
-        placeholder="@stud.uni-saarland.de"
-        id="Email"
-        required
-        @change="checkEmail"
-        v-model="email"
-      />
-      <p
-        class="errorMassage"
-        :class="{
-          disable: !emailValid,
-          disable: emailValid,
-        }"
-        id="emailError"
-      >
-        Geben Sie eine gültige E-Mail-Adresse für Studenten ein.
-      </p>
-    </div>
-
+  <!-- eslint-disable -->
+ <!-- prettier-ignore -->
+  
+              <form action="" class="form" method="POST">
+                
+                  <div class="form__group">
+                    <label for="User" class="form__label">Studenten-E-Mail:</label>
+                    <input
+                      type="text"
+                      placeholder="@stud.uni-saarland.de"
+                      id="User"
+                      class="form__input valid"
+                      name="email"
+                      v-model="User.email"
+                    />
+                  </div>
+                  
     <div class="form__group">
       <div class="linking">
-        <label for="Password" class="form__label">Kennwort:</label>
-        <!-- <a href="#" class="linking__forgetPassword">Forget password?</a> -->
-      </div>
-      <input
-        type="password"
-        class="form__input valid"
-        placeholder="8+ Characters"
-        id="Password"
-        required
-        @change="
-          passwordTouched = true;
-          checkPassword();
-        "
-        @keyup="checkPassword"
-        v-model="password"
-      />
-      <p class="errorMassage disable" id="passwordError">
-        Das Passwort muss mindestens 8 Zeichen lang sein.
-      </p>
-    </div>
-    <button
-      class="form__btn"
-      id="submit"
-      :disabled="!isValid"
-      :class="{ avalible: isValid, notAvalible: !isValid }"
-    >
-      Einloggen
-    </button>
-  </form>
-  <div class="linking">
+                    <label for="User" class="form__label">Kennwort:</label>
+                     </div>
+                     <input
+                      type="password"
+                      placeholder="*****"
+                      class="form__input valid"
+                      name="password"
+                      v-model="User.password"
+                    />
+                  </div>
+                
+           
+                    <button
+                      class="form__btn"
+                      @click.prevent="onLogin()"
+                    >
+                      Einloggen
+                    </button>
+                  
+              </form>
+             <div class="linking">
     <p for="Password" class="form__label">
      
     </p>
@@ -71,74 +46,42 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  name: "loginForm",
+  name: "Home",
   data() {
     return {
-      email: "",
-      password: "",
-      emailValid: true,
-      passwordValid: false,
-
-      passwordTouched: false,
-
-      accounts: [
-        { email: "pafasi@test.com", password: "pafasitest" },
-      ],
+      User: {
+        email: null,
+        password: null
+      }
     };
   },
-
   methods: {
-    checkEmail() {
-      /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(this.email)
-        ? (this.emailValid = true)
-        : (this.emailValid = false);
-    },
-    checkUpperCase(str) {
-      return /[A-Za-z0-9]/.test(str) ;
-    },
-    checkPassword() {
-      const userName = this.email.split("@")[0];
-      if (
-        this.password.length >= 8 &&
-        this.checkUpperCase(this.password) &&
-        this.password !== userName
-      ) {
-        document.getElementById("Password").classList.remove("notValid");
-        document.getElementById("Password").classList.add("valid");
-        document.getElementById("passwordError").classList.add("disable");
-        this.passwordValid = true;
-      } else if (this.passwordTouched) {
-        document.getElementById("Password").classList.remove("valid");
-        document.getElementById("Password").classList.add("notValid");
-        document.getElementById("passwordError").classList.remove("disable");
-        this.passwordValid = false;
-      }
-    },
-    checkLogin(e) {
-      e.preventDefault();
-      const status = this.accounts.filter(
-        (record) =>
-          record.email === this.email && record.password === this.password
-      );
-
-      if (status.length > 0) {
-        localStorage.setItem("userEmail", this.email);
-        this.$router.push("/main");
-      } else {
-        document.querySelector(".errorLogin").classList.remove("disable");
-        this.email = "";
-        this.password = "";
-        this.passwordValid = false;
-      }
-    },
-  },
-
-  computed: {
-    isValid() {
-      return this.emailValid && this.passwordValid && this.email;
-    },
-  },
+    onLogin() {
+      var data = new FormData();
+      data.append("email", this.User.email);
+      data.append("password", this.User.password);
+      axios
+        .post(
+          "http://localhost/patient-simulator/src/Api/api.php?action=login",
+          data
+        )
+        .then(res => {
+          if (res.data.error) {
+            console.log("Error", res.data);
+            alert(res.data.message);
+          } else {
+            console.log("Success", res.data.message);
+           
+            this.$router.push("/main");
+          }
+        })
+        .catch(err => {
+          console.log("Error", err);
+        });
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -196,7 +139,7 @@ a:focus {
   }
   &__btn {
     width: 100%;
-    @include customBorder($color__primary, $color__secondary);
+    @include customBorder($color__links, $color__secondary);
     color: $color__white;
     padding: 1rem;
     border-radius: 4px;
@@ -206,41 +149,10 @@ a:focus {
   }
 }
 
-.errorLogin {
-  border-radius: 4px;
-  margin: 0.5rem 0;
-  padding: 1rem 1.5rem;
-  @include customBorder(#fbe6e1, $color__gray--dark);
-  &__text {
-    font-size: 1.5rem;
-    color: $color__gray--dark;
-  }
-}
-.errorMassage {
-  color: $color__warning;
-  font-size: 1.2rem;
-  margin-top: 1rem;
-}
-.notAvalible {
-  background-color: $color__gray--light;
-  cursor: default;
-}
-.avalible {
-  background-color: $color__links;
-  cursor: pointer;
-}
 
-.notValid,
-.notValid:focus,
-.notValid:active {
-  border-color: $color__warning;
-}
 .valid:focus,
 .valid:active {
   border-color: $color__links;
   box-shadow: inset 0 0 4px 0 $color__links;
-}
-.disable {
-  display: none;
 }
 </style>
